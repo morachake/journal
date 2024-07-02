24073aa82b9f2c96a5ad3d7f8bc0367770d178da
morachake <jacobmoracha@gmail.com>
2024-07-02 15:55:24 +0300

    adding models

--- journal_api/admin.py
+++ journal_api/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import JournalEntry, Category
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user')
+    search_fields = ('name',)
+    list_filter = ('user',)
+
+    def save_model(self, request, obj, form, change):
+        if not obj.pk:
+            obj.user = request.user
+        super().save_model(request, obj, form, change)
+
+@admin.register(JournalEntry)
+class JournalEntryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'category', 'date')
+    search_fields = ('title', 'content')
+    list_filter = ('category', 'date', 'user')
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'content', 'category', 'date')
+        }),
+    )
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "category":
+            kwargs["queryset"] = Category.objects.filter(user=request.user)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+    def save_model(self, request, obj, form, change):
+        if not obj.pk:
+            obj.user = request.user
+        super().save_model(request, obj, form, change)
--- journal_api/models.py
+++ journal_api/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
-    
+
     def __str__(self):
         return self.name
 
@@ -12,7 +12,7 @@ class JournalEntry(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     content = models.TextField()
-    category = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='entries')
     date = models.DateField()
 
     def __str__(self):
