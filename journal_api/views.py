57df7cba13c84963643629b004c3e19999d9bdab
morachake <jacobmoracha@gmail.com>
2024-07-02 14:41:42 +0300

    adding swagger for api documentation

--- journal_api/views.py
+++ journal_api/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 from .models import JournalEntry
 from .serializers import UserSerializer, JournalEntrySerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 class UserCreate(generics.CreateAPIView):
     queryset = User.objects.all()
@@ -15,9 +17,14 @@ class JournalEntryListCreate(generics.ListCreateAPIView):
     serializer_class = JournalEntrySerializer
     permission_classes = [permissions.IsAuthenticated]
 
+    @swagger_auto_schema(operation_description="Get a list of journal entries", responses={200: JournalEntrySerializer(many=True)})
     def get_queryset(self):
+        # Short-circuit for swagger_fake_view
+        if getattr(self, 'swagger_fake_view', False):
+            return JournalEntry.objects.none()
         return JournalEntry.objects.filter(user=self.request.user)
 
+    @swagger_auto_schema(operation_description="Create a new journal entry")
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
@@ -25,5 +32,9 @@ class JournalEntryDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = JournalEntrySerializer
     permission_classes = [permissions.IsAuthenticated]
 
+    @swagger_auto_schema(operation_description="Retrieve, update or delete a journal entry", responses={200: JournalEntrySerializer()})
     def get_queryset(self):
+        # Short-circuit for swagger_fake_view
+        if getattr(self, 'swagger_fake_view', False):
+            return JournalEntry.objects.none()
         return JournalEntry.objects.filter(user=self.request.user)
